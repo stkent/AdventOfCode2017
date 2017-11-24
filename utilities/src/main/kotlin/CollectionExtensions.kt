108f51137f0921de14aff714845974c9bd230b86
Stuart Kent <skent.dev@gmail.com>
2017-11-24 17:52:03 -0500

    Clean up collection/set extensions

--- utilities/src/main/kotlin/CollectionExtensions.kt
+++ utilities/src/main/kotlin/CollectionExtensions.kt
@@ -1,25 +1,43 @@
-import java.util.Comparator
+fun <E> Collection<E>.permutations(): Set<List<E>> {
+    if (isEmpty()) return emptySet()
 
-fun <K> Collection<K>.elementCounts(): Map<K, Int> = groupBy({ it }).mapValues { it.value.size }
+    if (size == 1) return setOf(listOf(first()))
 
-fun <T : Comparable<T>> Collection<T>.highestFrequencyElement(tieBreaker: Comparator<T>): T {
-    return elementCounts()
-            .entries
-            .toSortedSet(
-                    compareByDescending(selector = Map.Entry<T, Int>::value)
-                            .thenBy(selector = Map.Entry<T, Int>::key, comparator = tieBreaker)
-            )
-            .first()
-            .key
+    val result = mutableSetOf<List<E>>()
+
+    for (element in this) {
+        (this - element).permutations().forEach {
+            val resultList = mutableListOf(element)
+            resultList.addAll(it)
+            result.add(resultList)
+        }
+    }
+
+    return result
 }
 
-fun <T : Comparable<T>> Collection<T>.lowestFrequencyElement(tieBreaker: Comparator<T>): T {
+fun <E> Collection<E>.elementCounts(): Map<E, Int> = groupBy({ it }).mapValues { it.value.size }
+
+fun <E> Collection<E>.highestFrequencyElements(): Set<E> {
+    if (isEmpty()) return emptySet()
+
+    val elementCounts = elementCounts()
+    val highestFrequency = elementCounts.map { it.value }.max()
+
     return elementCounts()
-            .entries
-            .toSortedSet(
-                    compareBy(selector = Map.Entry<T, Int>::value)
-                            .thenBy(selector = Map.Entry<T, Int>::key, comparator = tieBreaker)
-            )
-            .first()
-            .key
+            .filter { it.value == highestFrequency }
+            .map { it.key }
+            .toSet()
+}
+
+fun <E> Collection<E>.lowestFrequencyElements(): Set<E> {
+    if (isEmpty()) return emptySet()
+
+    val elementCounts = elementCounts()
+    val lowestFrequency = elementCounts.map { it.value }.min()
+
+    return elementCounts()
+            .filter { it.value == lowestFrequency }
+            .map { it.key }
+            .toSet()
 }
--- utilities/src/main/kotlin/SetExtensions.kt
+++ utilities/src/main/kotlin/SetExtensions.kt
@@ -1,21 +1,3 @@
-fun <E> Set<E>.permutations(): Set<List<E>> {
-    if (isEmpty()) return emptySet()
-
-    if (size == 1) return setOf(listOf(first()))
-
-    val result = mutableSetOf<List<E>>()
-
-    for (element in this) {
-        (this - element).permutations().forEach {
-            val resultList = mutableListOf(element)
-            resultList.addAll(it)
-            result.add(resultList)
-        }
-    }
-
-    return result
-}
-
 fun <E> Set<E>.powerSet(): Set<Set<E>> {
     return when (size) {
         0 -> setOf(emptySet())
