efd4d14b7a678280e9cedbfa17a02e668884c454
Stuart Kent <skent.dev@gmail.com>
2017-11-24 17:57:35 -0500

    Reformat all code

--- day01/src/main/kotlin/Main.kt
+++ day01/src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 class Main {
 
-    companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            println("hello, world!")
-        }
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      println("hello, world!")
     }
+  }
 
 }
--- utilities/src/main/kotlin/SetExtensions.kt
+++ utilities/src/main/kotlin/SetExtensions.kt
@@ -1,16 +1,17 @@
 fun <E> Set<E>.powerSet(): Set<Set<E>> {
-    return when (size) {
-        0 -> setOf(emptySet())
-        1 -> setOf(this, emptySet())
-        else -> {
-            val anyElement = iterator().next()
+  return when (size) {
+    0 -> setOf(emptySet())
+    1 -> setOf(this, emptySet())
 
-            val exclusiveRemainderPowerSet = minus(anyElement).powerSet()
+    else -> {
+      val anyElement = iterator().next()
 
-            val inclusiveRemainderPowerSet = emptySet<Set<E>>().toMutableSet()
-            exclusiveRemainderPowerSet.mapTo(inclusiveRemainderPowerSet) { it.union(setOf(anyElement)) }
+      val exclusiveRemainderPowerSet = minus(anyElement).powerSet()
 
-            return exclusiveRemainderPowerSet.union(inclusiveRemainderPowerSet)
-        }
+      val inclusiveRemainderPowerSet = emptySet<Set<E>>().toMutableSet()
+      exclusiveRemainderPowerSet.mapTo(inclusiveRemainderPowerSet) { it.union(setOf(anyElement)) }
+
+      return exclusiveRemainderPowerSet.union(inclusiveRemainderPowerSet)
     }
+  }
 }
