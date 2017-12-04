ae9c060376d3f5a0bb06c351e0ef7220ef1e3929
Stuart Kent <skent.dev@gmail.com>
2017-12-03 20:58:02 -0500

    day04+: prep

---
+++ day17/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+  import java.io.File
+
+class Main {
+
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+//      val input = File(Main::class.java.getResource("input.txt").file).readLines()
+//      val input =
+
+    }
+  }
+
+}
