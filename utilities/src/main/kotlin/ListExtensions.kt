22654605cb9c35a3cbb23b55faa9700ba7a91c46
Stuart Kent <skent.dev@gmail.com>
2017-12-16 11:50:04 -0600

    Clean up list extension

--- utilities/src/main/kotlin/ListExtensions.kt
+++ utilities/src/main/kotlin/ListExtensions.kt
@@ -1,6 +1,5 @@
-// Rotates a list to the left.
+// Rotates a List to the left.
 fun <E> List<E>.rotate(steps: Int): List<E> {
   val nSteps = steps.nonNegativeRem(size)
-
   return takeLast(size - nSteps) + take(nSteps)
 }
