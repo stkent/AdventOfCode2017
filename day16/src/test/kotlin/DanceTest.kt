9b9d1dd1b17e951db9a18f6e5ce218efcb0b2819
Stuart Kent <skent.dev@gmail.com>
2017-12-18 12:14:19 -0500

    day16: neaten tests

--- day16/src/test/kotlin/DanceTest.kt
+++ day16/src/test/kotlin/DanceTest.kt
@@ -3,49 +3,49 @@ import io.kotlintest.specs.BehaviorSpec
 
 class DanceTest : BehaviorSpec({
 
-  Given ("") {
+  Given ("a dance consisting of a single spin") {
     val dance = Dance("abcde", listOf("s1"))
 
-    When("") {
+    When("we compute the final program order after performing that dance once") {
       val finalOrder = dance.computeFinalOrder(repetitions = 1)
 
-      Then("") {
+      Then("the order should be correct") {
         finalOrder shouldBe "eabcd"
       }
     }
   }
 
-  Given ("") {
+  Given ("a dance consisting of a single exchange") {
     val dance = Dance("eabcd", listOf("x3/4"))
 
-    When("") {
+    When("we compute the final program order after performing that dance once") {
       val finalOrder = dance.computeFinalOrder(repetitions = 1)
 
-      Then("") {
+      Then("the order should be correct") {
         finalOrder shouldBe "eabdc"
       }
     }
   }
 
-  Given ("") {
+  Given ("a dance consisting of a single partner") {
     val dance = Dance("eabdc", listOf("pe/b"))
 
-    When("") {
+    When("we compute the final program order after performing that dance once") {
       val finalOrder = dance.computeFinalOrder(repetitions = 1)
 
-      Then("") {
+      Then("the order should be correct") {
         finalOrder shouldBe "baedc"
       }
     }
   }
 
-  Given ("") {
+  Given ("a short dance") {
     val dance = Dance("abcde", listOf("s1","x3/4","pe/b"))
 
-    When("") {
+    When("we compute the final program order after performing that dance once") {
       val finalOrder = dance.computeFinalOrder(repetitions = 1)
 
-      Then("") {
+      Then("the order should be correct") {
         finalOrder shouldBe "baedc"
       }
     }
